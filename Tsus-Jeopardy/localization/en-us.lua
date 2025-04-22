72c5f48d080a7ac04aad414b8a629dd91a280068
Tsutje <xundev90@gmail.com>
2025-04-22 17:05:23 +0800

    Add files via upload

---
+++ Tsus-Jeopardy/localization/en-us.lua
@@ -0,0 +1,306 @@
+return{
+    dictionary={
+        cami_charge="CHARGE!",
+        cami_retreat="RETREAT!",
+        usagi_laugh="Uwa-ha!",
+        usagi_laugh2="Iya-ha!",
+        usagi_laugh3="Waa-ha!",
+        comedy_succ="Friggin Sweet!",
+        comedy_blow="Crack.",
+        comedy_stoned="Stoned!",
+        comedy_loser="Capiche!",
+        psycro_planet="It's Singing.",
+        psycro_seal="It's Crying.",
+        psycro_die="Im Sorry.",
+        psycro_idle="...",
+        blank_rolled="!",
+        blank_failed="?",
+        blank_idle="..."
+    },
+    descriptions={
+        Back = {
+            b_tje_hexdeck = {
+                name = 'Hexed Deck',
+                text = {
+                    "Start with a {C:tsusjeopardytxt}Tsu's Jeopardy{}",
+                    "{C:tarot}Legendary{} {C:attention}Joker{} or",
+                    "{C:hextxt}Hexed Legendary{} {C:attention}Joker{}",
+                    "{X:hextxt,C:white}ALOT{} {X:hextxt,C:white}of{} {X:hextxt,C:white}Downsides{}",
+                    "{s:0.75,C:tsusjeopardytxt}Good Luck! <3{}"
+                }
+            },
+            b_tje_phonydeck = {
+                name = 'Phony Deck',
+                text = {
+                    "Start with a {C:tsusjeopardytxt}Tsu's Jeopardy{}",
+                    "{C:tarot}Legendary{} {C:attention}Joker{} or",
+                    "{C:hextxt}Hexed Legendary{} {C:attention}Joker{}"
+                }
+            }
+        },
+        Joker = {
+            j_tje_Kusuri = {
+                name = 'Kusuri',
+                text = {
+                    "{C:inactive}[Active Drug]",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:1.25,C:inactive}None!",
+                    "{C:inactive}-------------------------------------{}",
+                    "{C:inactive}[Effect]",
+                    "{C:attention}Create{} a {C:green}random{} {C:thmtron}Potion{}",
+                    "When {C:attention}starting{} a {C:attention}Blind{}",
+                    "{s:0.85,C:inactive}[Needs Space]",
+                    "{C:inactive}-------------------------------------{}",
+                    '{s:0.85,C:inactive}"Yep, Yep!"{}'
+                }
+            },
+            j_tje_Kusuri2 = {
+                name = 'Kusuri',
+                text = {
+                    "{C:inactive}[Active Drug]",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:1.25,C:thmtron}The Human Magnet Medicinatron{}",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:0.65,C:inactive}(#1# Rounds Remaining)",
+                    "{C:inactive}[Effect]",
+                    "{C:chips}Chips{} and {C:mult}Mult{} are {C:thmtron}Multiplied{}",
+                    "to {C:thmtron}eachother{} then {C:attention}Halved{}",
+                    "{C:attention}after {C:blue}scoring{}",
+                    "{C:inactive}-------------------------------------{}",
+                    '{s:0.85,C:inactive}"Just like Crystal M***!"'
+                }
+            },
+            j_tje_Kusuri3krn = {
+                name = 'Karane',
+                text = {
+                    "{C:inactive}[Active Drug]",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:1.25,C:thmtron}Ultimate Ennui Destroyer{}",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:0.65,C:inactive}(#1# Rounds Remaining)",
+                    "{C:inactive}[Effect]",
+                    "{C:attention}Switch{} Kusuri's {C:blue}Soul{}",
+                    "With a {C:green}random{} Girlfriend!",
+                    "{C:inactive,s:0.85}[Karane]",
+                    "Played {C:hearts}Hearts{} are turned into {C:spades}Spades{}",
+                    "Gains {X:mult,C:white}X#6#{} when triggered.",
+                    "{C:inactive,s:0.75}Currently{} {s:0.75,C:white,X:mult}X#5#{}",
+                    "{C:inactive}-------------------------------------{}",
+                    '{s:0.85,C:inactive}"JUST GET ME OUT OF THIS STUPID BODY ALREADY!"'
+                }
+            },
+            j_tje_Kusuri3hkr = {
+                name = 'Hakari',
+                text = {
+                    "{C:inactive}[Active Drug]",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:1.25,C:thmtron}Ultimate Ennui Destroyer{}",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:0.65,C:inactive}(#1# Rounds Remaining)",
+                    "{C:inactive}[Effect]",
+                    "{C:attention}Switch{} Kusuri's {C:blue}Soul{}",
+                    "With a {C:green}random{} Girlfriend!",
+                    "{C:inactive,s:0.85}[Hakari]",
+                    "Playing a {C:attention}Pair{} turns all",
+                    "{C:spades}Spades{} in {C:blue}Hand{} into {C:hearts}Hearts{}",
+                    "{C:money}$#4#{} when triggered",
+                    "{C:inactive,s:0.75}Currently{} {C:money,s:0.75}$#3#",
+                    "{C:inactive}-------------------------------------{}",
+                    '{s:0.85,C:inactive}"...Do you perhaps like them smaller?"'
+                }
+            },
+            j_tje_Kusuri3szk = {
+                name = 'Shizuka',
+                text = {
+                    "{C:inactive}[Active Drug]",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:1.25,C:thmtron}Ultimate Ennui Destroyer{}",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:0.65,C:inactive}(#1# Rounds Remaining)",
+                    "{C:inactive}[Effect]",
+                    "{C:attention}Switch{} Kusuri's {C:blue}Soul{}",
+                    "With a {C:green}random{} Girlfriend!",
+                    "{C:inactive,s:0.85}[Shizuka]",
+                    "Selecting a {C:attention}#4#{}",
+                    "{C:blue}Levels{} it by {C:blue}#3#",
+                    "{C:inactive,s:0.75}[Changes when triggered]{}",
+                    "{C:inactive}-------------------------------------{}",
+                    '{s:0.85,C:inactive}"Tis a pleasure to make thine acquaintance!"'
+                }
+            },
+            j_tje_Kusuri3nno = {
+                name = 'Nano',
+                text = {
+                    "{C:inactive}[Active Drug]",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:1.25,C:thmtron}Ultimate Ennui Destroyer{}",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:0.65,C:inactive}(#1# Rounds Remaining)",
+                    "{C:inactive}[Effect]",
+                    "{C:attention}Switch{} Kusuri's {C:blue}Soul{}",
+                    "With a {C:green}random{} Girlfriend!",
+                    "{C:inactive,s:0.85}[Nano]{}",
+                    "{C:blue}Played{} cards are turned into",
+                    "{C:attention}Ace King Queen Jack 10",
+                    "{C:attention}Respectively{} on their order",
+                    "{X:mult,C:white}X#2#{} for each card {C:attention}Changed{}",
+                    "{C:inactive,s:0.75}Currently{} {C:white,X:mult,s:0.75}X#1#{}",
+                    "{C:inactive}-------------------------------------{}",
+                    '{s:0.85,C:inactive}"High Straights are far more efficient."'
+                }
+            },
+            j_tje_Kusuri3hhr = {
+                name = 'Hahari',
+                text = {
+                    "{C:inactive}[Active Drug]",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:1.25,C:thmtron}Ultimate Ennui Destroyer{}",
+                    "{C:inactive}-------------------------------------{}",
+                    "{s:0.65,C:inactive}(#1# Rounds Remaining)",
+                    "{C:inactive}[Effect]",
+                    "{C:attention}Switch{} Kusuri's {C:blue}Soul{}",
+                    "With a {C:green}random{} Girlfriend!",
+                    "{C:inactive,s:0.85}[Hahari]{}",
+                    "{C:blue}Played{} cards are turned into",
+                    "{C:attention}2{}s",
+                    "{C:attention}2{}s Scored give {X:mult,C:white}X#1#{}",
+                    "{C:blue}Held in hand{} {C:attention}2{}s",
+                    "Give {C:money}${} and {C:attention}Increase{} {C:money}${} by {X:money,C:white}X#3#{}",
+                    "{s:0.65,C:inactive}[Increase resets when playing hand]",
+                    "{C:inactive}-------------------------------------{}",
+                    '{s:0.85,C:inactive}"Rentarou Sweetheart, I cant take it!~"'
+                }
+            },
+            j_tje_Schneider = {
+                name = 'Schneider',
+                text = {
+                    "At the {C:chips}Start{} of each {C:attention}Round{},",
+                    "{C:mult}Schneider{} {C:attention}records{} each card {C:attention}scored{}",
+                    "{C:hearts}Hearts{} {C:diamonds}Diamonds{} {C:clubs}Clubs{} and {C:spades}Spades{} give",
+                    "{X:mult,C:white}X#5#{} {C:money}$#6#{} {C:mult}#7# Mult{} and {C:chips}#8# Chips{} {C:attention}Respectively{}",
+                    '{s:0.75,C:inactive}Once per round{}',
+                    '{C:attention}After Scoring{} {C:chips}First Hand{}, {C:mult}Schneider{} takes a rest',
+                    '{C:mult}Schneider{} {c:attention}gains{} {X:mult,C:white}X0.1{} per scored {C:hearts}Hearts{}',
+                    '{X:mult,C:white,s:0.75}Xmult{} {C:inactive,s:0.75}only applies during{} {C:attention,s:0.75}resting{}',
+                    '{C:inactive}Currently {X:mult,C:white}X#3#{}',
+                    '{s:0.7,C:inactive}Aditionally, after defeating{} {s:0.7,C:attention}Boss Blind{}{s:0.7,C:inactive},{} {s:0.7,C:attention,s:0.7}Fill{} {C:inactive,s:0.7}your{} {s:0.7,C:attention}Consumable Slots{} {C:inactive,s:0.7}with{}',
+                    '{C:inactive,s:0.7}A{} {s:0.7,C:mult}Slice{} {C:inactive,s:0.7}of{} {s:0.7,C:attention}Life{}',
+                    '{C:inactive}"Who cares whether the shelter is the eave...{}',
+                    '{C:inactive}Or the rubble?"{}'
+                }
+            },
+            j_tje_Crystal = {
+                name = 'CRYSTA//',
+                text = {
+                    "{C:clstite}Purifies{} {C:mult}Polychrome{} {C:attention}Stone Cards{} to",
+                    "{C:clstite}Celestite{} {C:attention}when{} {C:blue}Played{} {C:attention}by itself.{}",
+                    "{C:inactive,s:0.75}[Must Have Room]{}",
+                    "{X:clstite,C:white}N^N2{} {C:mult}Mult{}",
+                    "{C:inactive,s:0.75}Where N is your played hands current level, and N2 is your current Charge.{}",
+                    "{X:clstite,C:white}Charge{} : {X:clstite,C:white}#5#{}",
+                    "{s:0.75,C:green}#1#{} {s:0.75,C:inactive}in{} {s:0.75,C:green}#2#{} {C:inactive,s:0.75}chance{} {s:0.75,C:inactive}to create a{} {s:0.75,C:mult}Polychrome{}",
+                    "{C:attention,s:0.75}Stone Card when{} {s:0.75,C:blue}Opening a Booster Pack.{}",
+                    '{C:inactive}"...Do I talk too much?"{}'
+                }
+            },
+            j_tje_summersend = {
+                name = "Summer's End",
+                text = {
+                    "#1# in #2#"
+                }
+            },
+            j_tje_mineturtle = {
+                name = "Mine Turtle",
+                text = {
+                    "{X:mult,C:white}X#3#",
+                    "{C:green}#1#{} in {C:green}#2#{} chance to",
+                    "{C:attention}Triple{} {C:mult}Blind size{}",
+                    "{C:inactive}-------------{}",
+                    '{s:0.85,C:inactive}"Hello!"{}'
+                }
+            },
+            j_tje_tsunova = {
+                name = "Tsunova Express",
+                text = {
+                    "{C:green}#1#{} in {C:green}#2#{} chance to",
+                    "{C:attention}Create{} a {C:green}random{}",
+                    "{C:planet}Planet{} when using a",
+                    "non {C:planet}Planet{} {C:attention}Consumable{}",
+                    "{C:green}#1#{} in {C:green}#3#{} chance to also",
+                    "{C:red}level down{} most played hand when",
+                    "using a {C:planet}Planet{} {C:attention}Consumable{}",
+                    "{C:inactive,s:0.75}[Needs Space]{}",
+                    "{C:inactive}----------------------------{}",
+                    '{s:0.85,C:inactive}"Planetary expeditions are my favorite!"{}'
+                }
+            },
+            j_tje_hauntinglysweetvices = {
+                name = "Hauntingly Sweet Vices",
+                text = {
+                    "{C:chips}+#3# Chips{} {C:mult}+#4# Mult{}",
+                    "{C:green}#1#{} in {C:green}#2#{} chance to {C:attention}Decrease{} by",
+                    "{C:chips}-#5# Chips{} {C:mult}-#6# Mult{} at end of round",
+                    "{s:0.75,C:inactive}[3 Rolls]{}",
+                    "{C:inactive}-------------------------{}",
+                    '{s:0.85,C:inactive}"Tight bars little man."{}'
+                }
+            },
+            j_tje_dacnovania = {
+                name = "Dacnovania",
+                text = {
+                    "{C:attention}Stores{} half of {C:blue}played hands{} {C:mult}Mult{}",
+                    "{C:green}#1#{} in {C:green}#3#{} chance to {C:attention}Score{} stored {C:mult}Mult{}",
+                    "{C:green}#1#{} in {C:green}#2#{} chance to {C:mult}Halve{} stored {C:mult}Mult{}",
+                    "at end of round",
+                    "{C:inactive,s:0.75}Currently{} {C:mult,s:0.75}#4# Mult{} {C:inactive,s:0.75}stored{}",
+                    "{C:inactive}-----------------------------{}",
+                    '{s:0.85,C:inactive}*tch.{}'
+                }
+            },
+            j_tje_nomorenuzzles = {
+                name = "No More Nuzzles",
+                text = {
+                    "when no {C:mult}Discards{} were used",
+                    "this gains {C:attention}1{} trigger of",
+                    "{X:mult,C:white}X#1#{} by end of round",
+                    "Lose {C:attention}1{} trigger when {C:mult}Discarding{}",
+                    "{C:inactive,s:0.75}Currently{} {C:attention,s:0.75}#2# triggers{}",
+                    "{C:inactive}-----------------------------{}",
+                    '{s:0.5,C:inactive}"Toby Fox composing the greatest song ever made using only"{}',
+                    '{s:0.5,C:inactive}"The Scrimblo Bimblos Scrunky Adventure soundfont"{}',
+                }
+            },
+            j_tje_cookiecat = {
+                name = "Cookie Cat",
+                text = {
+                    "{C:green}#1#{} in {C:green}#2#{} chance to",
+                    "{C:blue}level up{} played hand",
+                    "{C:green}#1#{} in {C:green}#2#{} to also {C:red}lose{} {C:money}$2{}",
+                    "{C:inactive,s:0.75}[#3# Hands Remaining]{}",
+                    "{C:inactive}-------------------------{}",
+                    '{s:0.85,C:inactive}"He left his family behind!"{}'
+                }
+            },
+            j_tje_intertwiningluck = {
+                name = "Intertwining Luck",
+                text = {
+                    "When starting {C:attention}Blind{}",
+                    "{C:money}-$#2#{}",
+                    "{C:green}#1#{} in {C:green}#3#{} chance for an",
+                    "{C:uncommon}Uncommon{} {C:attention}Joker{}",
+                    "{C:green}#1#{} in {C:green}#4#{} chance for a",
+                    "{C:rare}Rare{} {C:attention}Joker{}",
+                    "{C:green}#1#{} in {C:green}#5#{} chance for a",
+                    "{C:legendary}Legendary{} {C:attention}Joker{}",
+                    "{C:inactive,s:0.75}[Needs Space]{}",
+                    "{C:inactive,s:0.75}[{}{C:rare,s:0.75}Rare{} {C:inactive,s:0.75}Pity: #6#]{}{C:inactive,s:0.75} [{}{C:legendary,s:0.75}Legendary{} {C:inactive,s:0.75}Pity: #7#]{}",
+                    "{C:inactive}-------------------------{}",
+                    '{s:0.85,C:inactive}"I Pulled a Qiqi~..."{}'
+                }
+            }
+        }
+        ----------------------------------Jokers
+        ----------------------------------HexedLegendaryJokers
+    }
+}
+-------------------Joker desc.---------------------
