3cb2590a48f0dbbbd5ecdf2fc0800306dd913fb7
Tsutje <xundev90@gmail.com>
2025-04-22 19:41:27 +0800

    Add files via upload

--- items/Jokers/Jokers2/cookiecat.lua
+++ items/Jokers/Jokers2/cookiecat.lua
@@ -9,26 +9,24 @@ SMODS.Joker{
     eternal_compat = false,
     perishable_compat = true,
     pos = {x = 3, y = 1},
-    config = {extra = {odds = 2, usesremaining = 5}},
+    config = {extra = {odds = 2, usesremaining = 3, checked = 0}},
     loc_vars = function(self, info_queue, card)
-        return {vars = {G.GAME.probabilities.normal or 1, card.ability.extra.odds, card.ability.extra.usesremaining}}
+        return {vars = {G.GAME.probabilities.normal or 1, card.ability.extra.odds, card.ability.extra.usesremaining, card.ability.extra.checked}}
     end,
     calculate = function(self, card, context)
-        if context.before and card.ability.extra.usesremaining ~= 0 then
-            card.ability.extra.usesremaining = card.ability.extra.usesremaining-1
+        if context.before then
             if pseudorandom('cookiecat') < G.GAME.probabilities.normal/card.ability.extra.odds then
                 card_eval_status_text(card, 'extra', nil, nil, nil, {message = ("Wooho!"), colour = HEX('F8BEC7')})
                 SMODS.calculate_effect({level_up = true}, card)        
-            end
-            if pseudorandom('cookiecat2') < G.GAME.probabilities.normal/card.ability.extra.odds then
+            elseif pseudorandom('cookiecat') < G.GAME.probabilities.normal/card.ability.extra.odds then
                 card_eval_status_text(card, 'extra', nil, nil, nil, {message = ("Aww.."), colour = HEX('FFEED1')})
                 ease_dollars(-2)
             end
-            if card.ability.extra.usesremaining == 0 and context.post_joker then
-                card:juice_up(0.3, 0.4)
-                G.jokers:remove_card(card)
-                card:remove()
-            end
+        elseif context.end_of_round and card.ability.extra.checked == 0 then
+            card.ability.extra.checked = 1
+            card.ability.extra.usesremaining = card.ability.extra.usesremaining-1
+        elseif context.setting_blind then
+            card.ability.extra.checked = 0
         end
     end
 }
